c16de7bbd874f3a64be22300ee32997546f2d60d
brenda0323manon <brendamanon0526@gmail.com>
2024-01-30 14:38:43 -0500

    edit changed on package-json

--- src/server.js
+++ src/server.js
@@ -27,29 +27,6 @@ app.post('/QuizPage', async (req, res) => {
 
   res.write(JSON.stringify(completion.choices[0].message.content));
 
-  // try {
-  //   const response = await axios.post(apiUrl, {
-  //     messages: (req.body),
-  //     type: 'text',
-  //     model: 'gpt-3.5-turbo', // Specify the model to use
-  //   }, {
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Authorization': 'Bearer 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',
-  //     },
-  //   });
-
-  //   if (!response.data || !response.data.choices || !response.data.choices[0]) {
-  //     throw new Error('Failed to fetch quiz data');
-  //   }
-
-  //   const generatedQuiz = response.data.choices[0].text;
-
-  //   res.json({ generatedQuiz });
-  // } catch (error) {
-  //   console.error('Error in /QuizPage endpoint:', error.response ? error.response.data : error.message);
-  //   res.status(500).json({ error: 'Internal Server Error' });
-  // }
 });
 
 app.listen(PORT, () => {
@@ -62,56 +39,3 @@ app.listen(PORT, () => {
 
 
 
-
-
-
-// const express = require('express');
-// const express = require('express');
-// const cors = require('cors'); 
-// const OpenAI = require("openai");
-// import axios from 'axios'; // Import axios library
-// const bodyParser = require('body-parser');
-// // const fetch = require('node-fetch'); // Import the fetch function
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use(cors()); // Enable CORS
-
-// const openai = new OpenAI({
-//   organization: 'org-fr67CpSt024fAgyOEDU81CfU',
-// });
-
-// app.use(bodyParser.json());
-
-// app.post('/QuizPage', async (req, res) => {
-//   const apiUrl = 'https://api.openai.com/v1/chat/completions';
-
-//   try {
-//     const response = await axios.post(apiUrl, req.body, {
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Bearer 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',
-//       },
-//     });
-
-//     if (!response.ok) {
-//       throw new Error('Failed to fetch quiz data');
-//     }
-
-//     const responseData = await response.json();
-
-//     // Extract relevant information from the API response
-//     const generatedQuiz = responseData.choices[0].text;
-
-//     // Send the extracted information back to the React app
-//     res.json({ generatedQuiz });
-//   } catch (error) {
-//     console.error('Error:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
